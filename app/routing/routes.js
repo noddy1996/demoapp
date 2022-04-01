caf05d227d4a93b7dd142380866ea6afdcaf9e13
gaurav <devgaurav700@gmail.com>
2022-04-01 11:39:35 +0530

    details scrreen complete

--- app/routing/routes.js
+++ app/routing/routes.js
@@ -1,14 +1,15 @@
+import DetailsScreen from '../screens/Details';
 import Home from '../screens/Home';
 
 export const RoutesList = [
-  
   {
     name: 'Home',
     component: Home,
-    options: {
-      tabBarBadge: 3,
-      tabBarLabel: 'Home',
-      //.... rest are the params for Tab.Screen See : https://reactnavigation.org/docs/bottom-tab-navigator/#options
-    },
+    options: {},
+  },
+  {
+    name: 'Details',
+    component: DetailsScreen,
+    options: {},
   },
 ];
